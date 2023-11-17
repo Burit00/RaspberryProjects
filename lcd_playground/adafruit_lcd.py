4e27a0efb9bf88026302fc78f7e659cfa39b6201
Burit00 <burciak01@gmail.com>
2023-11-17 13:56:34 +0100

    LCD_Playground: print "Hello World!" test 9.1

--- lcd_playground/adafruit_lcd.py
+++ lcd_playground/adafruit_lcd.py
@@ -6,7 +6,7 @@ LCD_ROWS = 2
 
 lcd = Adafruit_CharLCD(rs=LCD_RS, en=LCD_E,
                        d4=LCD_D4, d5=LCD_D5, d6=LCD_D6, d7=LCD_D7,
-                       cols=LCD_COLS, rows=LCD_ROWS
+                       cols=LCD_COLS, lines=LCD_ROWS
                        )
 def main():
     lcd.clear()
