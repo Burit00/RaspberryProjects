e430d0479d797ee83d2cb32e851ac6779b57f74c
Burit00 <burciak01@gmail.com>
2023-11-17 14:22:03 +0100

    LCD_Playground: print "Hello World!" test 9.2

--- lcd_playground/adafruit_lcd.py
+++ lcd_playground/adafruit_lcd.py
@@ -1,5 +1,6 @@
 from board_pinout import *
 from Adafruit_CharLCD import Adafruit_CharLCD
+from time import sleep
 
 LCD_COLS = 16
 LCD_ROWS = 2
@@ -8,6 +9,10 @@ lcd = Adafruit_CharLCD(rs=LCD_RS, en=LCD_E,
                        d4=LCD_D4, d5=LCD_D5, d6=LCD_D6, d7=LCD_D7,
                        cols=LCD_COLS, lines=LCD_ROWS
                        )
-def main():
+
+
+while True:
     lcd.clear()
-    #lcd.message("Chuj do dupy!")
+    lcd.message("Chuj do dupy!\nNiech to zacznie działać")
+    sleep(1)
+
